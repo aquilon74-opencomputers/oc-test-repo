2631e8a176cacac64eb1c49fbab079b5ad49267d
aquilon74 <aquilon74@gmail.com>
2021-01-09 21:30:59 +0100

    test

--- test.lua
+++ test.lua
@@ -1,1 +1,4 @@
-print("version 1")
+get a repo content: https://api.github.com/repos/{owner}/{repo}/contents
+get a raw file with a scpecific commit: https://raw.githubusercontent.com/{owner}/{repo}/{commit_hash}/{filename}
+get all commit: https://api.github.com/repos/repos/{owner}/{repo}/commits
+get all commit of a file: https://api.github.com/repos/{owner}/{repo}/commits?path={path}
